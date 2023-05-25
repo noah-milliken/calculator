54f6230eab5346d74cbcec6f1ccf54a055d36471
noah milliken <noahmilliken@gmail.com>
2023-05-25 10:03:51 -0400

    initial calculator functionality working

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('hello i am here to calculate numbers for you!')
+const display = document.getElementById('display')
+const buttons = document.querySelectorAll('.num-btn')
+const delBtn = document.getElementById('del-btn')
+const equal = document.getElementById('equals')
+let displayValue = ''
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        display.textContent += e.target.id
+        displayValue += e.target.id
+        console.log(displayValue)
+        console.log(display.textContent)
+    })
+})
+equal.addEventListener('click', function calculate() {
+    let opVal = displayValue.split(' ')
+        (operate(opVal[1], opVal[0], opVal[2]))
+
+})
+
+
+//function that uses reduce and calls the function at each itteration. 
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    if (operator === '+') {
+        return add(a, b)
+    } else if (operator === '-') {
+        return subtract(a, b)
+    } else if (operator === '*') {
+        return multiply(a, b)
+    } else if (operator === "/") {
+        return divide(a, b)
+    }
+}
