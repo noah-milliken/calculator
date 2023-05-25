7dc6b3ad32114551f6a0ca07743371ae3eb5bbd7
noah milliken <noahmilliken@gmail.com>
2023-05-25 11:52:35 -0400

    finished functionality will style more later

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
 const display = document.getElementById('display')
 const buttons = document.querySelectorAll('.num-btn')
-const delBtn = document.getElementById('del-btn')
 const equal = document.getElementById('equals')
+const clear = document.getElementById('clear')
+const delBtn = document.getElementById('del-btn')
+display.style.textAlign = 'right'
 let displayValue = ''
+console.log(buttons)
 
 
+console.log(clear)
 buttons.forEach(button => {
+
     button.addEventListener('click', function (e) {
+        console.log(e)
         display.textContent += e.target.id
         displayValue += e.target.id
-        console.log(displayValue)
-        console.log(display.textContent)
     })
 })
 equal.addEventListener('click', function calculate() {
+
     let opVal = displayValue.split(' ')
-        (operate(opVal[1], opVal[0], opVal[2]))
-
+    console.log(typeof opVal[2])
+    if (!opVal[1] || !opVal[2]) {
+        return
+    } else {
+        display.textContent = operate(opVal[1], opVal[0], opVal[2])
+        displayValue = operate(opVal[1], opVal[0], opVal[2])
+        console.log(display.textContent)
+    }
 })
+clear.addEventListener('click', function () {
+    displayValue = ''
+    display.textContent = ''
+})
+// delBtn.addEventListener('click', function () {
+//     displayValue = displayValue.slice(0, -1)
+//     display.textContent = displayValue = displayValue.slice(0, -1)
+//     console.log(displayValue)
+// })
 
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    if (operator === '/' && b === 0) {
+        console.log('You cant divide by 0 you donut')
+        //} else if () 
+        //
 
-//function that uses reduce and calls the function at each itteration. 
-
-
-
+    } else {
+        if (operator === '+') {
+            return add(a, b)
+        } else if (operator === '-') {
+            return subtract(a, b)
+        } else if (operator === '*') {
+            return multiply(a, b)
+        } else if (operator === "/") {
+            return divide(a, b)
+        }
+    }
+}
 function add(a, b) {
     return a + b
 }
@@ -37,18 +72,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
-}
-
-function operate(operator, a, b) {
-    a = Number(a)
-    b = Number(b)
-    if (operator === '+') {
-        return add(a, b)
-    } else if (operator === '-') {
-        return subtract(a, b)
-    } else if (operator === '*') {
-        return multiply(a, b)
-    } else if (operator === "/") {
-        return divide(a, b)
-    }
 }
